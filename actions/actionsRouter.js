034332f5e8754228970530169d8a20225622ea1e
MINE <Mine@MINEs-MacBook-Pro-2.local>
2018-05-04 12:04:17 -0700

    Added Delete Action

--- actions/actionsRouter.js
+++ actions/actionsRouter.js
@@ -44,7 +44,7 @@ router.post('/', (req, res) => {
 });
 
 // PUT/Update Action
-router.put(':id', (req, res) => {
+router.put('/:id', (req, res) => {
     const { id } = req.params;
     const update = req.body;
     actionModel
@@ -55,6 +55,24 @@ router.put(':id', (req, res) => {
         .catch(error => {
             res.status(500).json(`There was an error updating action: ${error}`);
         });
+});
+
+// DELETE/Delete Action
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    actionModel
+        .get(id)
+        .then(response => {
+            action = { ...response[0] };
+            actionModel
+                .remove(id)
+                .then(response => {
+                    res.status(200).json(action);
+                })
+        })
+                .catch(err => {
+                    res.status(500).json(`There was an error deleting project: ${error}`);
+                });
 });
 
 module.exports = router;
