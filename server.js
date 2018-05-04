105f91335adc708b4f53731cc0142c0d97c45e7e
MINE <Mine@MINEs-MacBook-Pro-2.local>
2018-05-04 10:51:37 -0700

    Created Projects & Actions folder along with JS files

--- server.js
+++ server.js
@@ -5,15 +5,26 @@
 // npm i knex
 // npm i sqlite3
 // yarn add nodemon --dev
+// npm i cors
 
 // Importing Node Modules
 const express = require('express');
-
 const helmet = require('helmet');
+const cors = require('cors');
+
+// const projectsRouter = require('./projects/projectsRouter.js');
+// const actionsRouter = require('./actions/actionsRouter.js');
 
 // Server Code
 const server = express();
 
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+
+// server.use('/api/projects', projectsRouter);
+// server.use('/api/actions', actionsRouter);
+
 const port = 5000;
 server.listen(port, () =>
-    console.log('\n== Amanda is listening to Port 5000 ==\n'));
+    console.log('Amanda is listening to Port 5000'));
