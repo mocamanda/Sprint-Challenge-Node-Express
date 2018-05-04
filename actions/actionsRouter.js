d37824677019c276ff9130d0bc22f198e38fa347
MINE <Mine@MINEs-MacBook-Pro-2.local>
2018-05-04 11:52:21 -0700

    GET/Read for Actions

--- actions/actionsRouter.js
+++ actions/actionsRouter.js
@@ -2,8 +2,19 @@ const express = require('express');
 
 const router = express.Router();
 
-const actionModel = require('.../data/helpers/actionModel.js');
+const actionModel = require('../data/helpers/actionModel.js');
 
-
+// /api/actions
+// GET/Read Actions
+router.get('/', (req, res) => {
+    actionModel
+        .get()
+        .then(actions => {
+            res.status(200).json(actions);
+        })
+        .catch(err => {
+            res.status(500).json(`There was an error getting actions: ${error}`);
+        });
+});
 
 module.exports = router;
