d5711516f5575149dbf3cf113ae6e53c511f62e1
MINE <Mine@MINEs-MacBook-Pro-2.local>
2018-05-04 11:26:31 -0700

    Create(POST), Read(GET), Update(PUT) added

--- projects/projectsRouter.js
+++ projects/projectsRouter.js
@@ -5,14 +5,55 @@ const router = express.Router();
 const projectModel = require('../data/helpers/projectModel.js');
 
 // /api/projects
+// GET/Read Project
 router.get('/', (req, res) => {
     projectModel
         .get()
         .then(projects => {
             res.status(200).json(projects);
         })
-        .catch(error => {
+        .catch(err => {
             res.status(500).json(`There was an error getting projects: ${error}`);
+        });
+});
+
+// GET/Read specific Project
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+
+    projectModel
+        .get(id)
+        .then(project => {
+            res.status(200).json(projects);
+        })
+        .catch(err => {
+            res.status(500).json(`There was an error getting that project: ${error}`);
+        });
+});
+
+// POST/Create Project
+router.post('/', (req, res) => {
+    projectModel
+        .insert(req.body)
+        .then(response => {
+            res.status(200).json(response);
+        })
+        .catch(err => {
+            res.status(500).json(`There was an error posting project: ${error}`);
+        });
+});
+
+// PUT/Update Project
+router.put('/:id', (req, res) => {
+    const { id } = req.params;
+    const update = req.body;
+    projectModel
+        .update(id, update)
+        .then(response => {
+            res.status(200).json(response);
+        })
+        .catch(error => {
+            res.status(500).json(err);
         });
 });
 
